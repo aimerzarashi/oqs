d34dfdd94e886cf12a38e26b817c657c1a7df0df
aimerzarashi <git.aimerzarashi.com>
2024-05-19 16:42:03 +0900

    added hello get method

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -10,6 +12,10 @@ func main() {
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+
+	e.GET("/hello", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
 	
 	e.Logger.Fatal(e.Start(":1323"))
 }
